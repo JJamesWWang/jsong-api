2ce90c1d0977e3e7285b559864ac9fc63f0ce8c9
James S. Wang <JJamesWWang@gmail.com>
2021-12-29 13:51:39 -0500

    Initial commit

---
+++ src/spotify.py
@@ -0,0 +1,49 @@
+import requests
+from decouple import config
+import base64
+from dataclasses import dataclass
+
+
+def get_access_token() -> str:
+    return requests.post(
+        "https://accounts.spotify.com/api/token",
+        data={"grant_type": "client_credentials"},
+        headers={
+            "Authorization": "Basic "
+            + base64.b64encode(
+                bytes(
+                    f"{config('SPOTIFY_CLIENT_ID')}:{config('SPOTIFY_CLIENT_SECRET')}",
+                    "utf-8",
+                )
+            ).decode("utf-8"),
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+    ).json()["access_token"]
+
+
+@dataclass
+class Track:
+    name: str
+    artists: list[str]
+
+
+@dataclass
+class Playlist:
+    name: str
+    tracks: list[Track]
+
+
+def get_playlist(access_token: str, playlist_link: str) -> Playlist:
+    playlist_id = playlist_link.split("/")[-1]
+    playlist_data = requests.get(
+        f"https://api.spotify.com/v1/playlists/{playlist_id}",
+        headers={"Authorization": f"Bearer {access_token}"},
+    ).json()
+    tracks = [
+        Track(
+            name=track["track"]["name"],
+            artists=[artist["name"] for artist in track["track"]["artists"]],
+        )
+        for track in playlist_data["tracks"]["items"]
+    ]
+    return Playlist(name=playlist_data["name"], tracks=tracks)
---
+++ src/spotifytest.py
@@ -0,0 +1,41 @@
+import pytest
+import spotify
+
+
+@pytest.fixture
+def playlist_link() -> str:
+    return (
+        "https://open.spotify.com/playlist/0mAgqUtF9rEbXuIGFlIK4I?si=f0d9a82224244fde"
+    )
+
+
+@pytest.fixture
+def access_token() -> str:
+    return spotify.get_access_token()
+
+
+@pytest.fixture
+def playlist(playlist_link, access_token) -> spotify.Playlist:
+    return spotify.get_playlist(access_token, playlist_link)
+
+
+def test_get_access_token():
+    assert isinstance(spotify.get_access_token(), str)
+
+
+def test_get_playlist(playlist):
+    assert isinstance(playlist, spotify.Playlist)
+
+
+def test_playlist_name(playlist):
+    assert playlist.name == "Test Playlist"
+
+
+def test_playlist_tracks_exist(playlist):
+    assert len(playlist.tracks) == 3
+
+
+def test_playlist_tracks_correct(playlist):
+    for track in playlist.tracks:
+        assert track.name in ["Electric Shock", "D-D-DANCE", "GLASSY"]
+        assert track.artists in [["f(x)"], ["IZ*ONE"], ["JO YURI"]]
