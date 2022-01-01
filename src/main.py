106fc8f2e50fc52eaf3f5505667bb694fb9ae706
James S. Wang <JJamesWWang@gmail.com>
2022-01-01 11:31:38 -0500

    Websocket: Move websocket to /routers

--- src/main.py
+++ src/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from .routers.websocket import router as websocket_router
+
 
 app = FastAPI()
 
+origins = ["http://localhost:3000"]
 
-@app.get("/")
-async def index():
-    return {"message": "Hello World"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(websocket_router)
