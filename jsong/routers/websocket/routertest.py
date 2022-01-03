dbd75b843fab14904b5d2a7d8ddc9940adb66acd
James S. Wang <JJamesWWang@gmail.com>
2022-01-02 19:23:42 -0500

    Websocket: Add tests for chat and claim host

--- jsong/routers/websocket/routertest.py
+++ jsong/routers/websocket/routertest.py
@@ -32,3 +32,40 @@ def test_websocket_connect():
         assert len(payload["uid"]) > 0
         assert payload["username"] == "hey"
         assert payload["isHost"] is False
+
+
+def test_websocket_disconnect():
+    with client.websocket_connect("/ws/hey") as websocket1:
+        with client.websocket_connect("/ws/hi") as websocket2:
+            pass
+        context = websocket1.receive_json()
+        ws1_connected = websocket1.receive_json()
+        ws2_connected = websocket1.receive_json()
+        disconnected = websocket1.receive_json()
+        payload = disconnected["payload"]
+        assert payload["username"] == "hi"
+
+
+def test_websocket_chat():
+    with client.websocket_connect("/ws/hey") as websocket1:
+        with client.websocket_connect("/ws/hi") as websocket2:
+            websocket2.send_json("hello")
+            context = websocket1.receive_json()
+            ws1_connected = websocket1.receive_json()
+            ws2_connected = websocket1.receive_json()
+            chat = websocket1.receive_json()
+            payload = chat["payload"]
+            assert payload["member"]["username"] == "hi"
+            assert payload["content"] == "hello"
+
+
+def test_websocket_claim_host():
+    with client.websocket_connect("/ws/hey") as websocket1:
+        with client.websocket_connect("/ws/hi") as websocket2:
+            context = websocket1.receive_json()
+            ws1_connected = websocket1.receive_json()
+            ws2_connected = websocket1.receive_json()
+            client.put(f"/lobby/host/{ws2_connected['payload']['uid']}")
+            claim_host = websocket1.receive_json()
+            payload = claim_host["payload"]
+            assert payload["username"] == "hi"
